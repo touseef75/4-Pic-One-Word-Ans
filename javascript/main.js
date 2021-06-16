3c933698f4e8eaf8071d0d54c9064cdb271b4b9f
touseef75 <imrantouseef776@gmail.com>
2021-06-16 10:07:29 -0700

    add local storage

--- javascript/main.js
+++ javascript/main.js
@@ -11,6 +11,7 @@
               "./images/joy.jpg",]
         
         let num = 0;
+        let num2 =0;
         let h2 = document.getElementById("h2");
         let h1 =document.getElementById("h1")
         let but = document.getElementById("but2")
@@ -19,13 +20,17 @@
             let inpu = document.getElementById("inpu").value;
             if(inpu == data[num]){
                 num++;
-                h1.innerHTML = data[num];
+                num2++;
+                h1.innerHTML = data[num] ;
                 img.src = imgs[num];
-                h2.innerHTML = num;
-            } 
+                h2.innerHTML = num2;
+            }if( h1.style.display = "block"){
+                h1.style.display = "none";
+
+        }
         })
         let count = 0;
-        let cou= document.getElementById("count");
+        let cou = document.getElementById("count");
 
 
         setInterval(run,1000)
@@ -35,3 +40,15 @@
             cou.innerHTML= count;
             console.log(count)
         }
+        let but1 = document.getElementById("but1");
+        but1.addEventListener("click",function(){
+            if(num2 >= 3){
+                h1.style.display = "block";
+                num2 = num2 - 3;
+            }else(
+                alert("you score must be greater then 3")
+            )
+        })
+        let play = document.getElementById("play");
+        let rel = localStorage.getItem('player');
+        play.innerHTML += rel ;
